608df6cd3fb4fc8b4917a5711de64271f7b58c63
thaboRach <masipathabo.rachidi@vcontractor.co.za>
2022-12-11 17:46:03 +0200

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import preprocess from "svelte-preprocess";
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import preprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/svelte-todo-list/',
   plugins: [
     svelte({
       preprocess: preprocess(),
